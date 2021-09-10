ee8d455f1e49671c23172dbacc9fd98ed84c70ea
gelo-gels <riangeloo17@gmail.com>
2021-09-10 10:49:19 +0800

    second deploy design

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,239 @@
 <template>
   <q-page class="font-poppins container__index">
+
+
+    <div class="text-bold q-mb-md text-center dateToday" style="font-size: 24px">
+        {{currentDateTime()}}
+    </div>
+
+
+
     <div class="row">
+
+
+
+
 
       <div class="col-md-4 col-sm-6 col-12">
         <div style="margin: 10px">
             <div class="ZoomBoxes">
               <div class="text-bold" style="font-size: 20px">
-                HFIL11
+                OLHEAP11
                 </div>
                 <div style="font-size: 10px">
-                  7:00 AM - 8:00 AM
+                  9:00 AM - 10:00 AM
+                </div>
+                <div style="font-size: 10px">
+                  Mondays
+                </div>
+
+              <div style="font-size: 13px">
+                <div class="q-mt-md">
+                  Meeting ID: 5255288358 <i class="far fa-copy" />
+                </div>
+                <div>
+                  Passcode: SHS4228 <i class="far fa-copy" />
+                </div>
+              </div>
+
+              <div class="text-center">
+                <div class="q-mt-md text-bold" style="font-size: 13px">
+                  DATES
+                </div>
+                <div style="font-size: 12px">
+                  <div> 
+                    September 13, 20, 27
+                  </div>
+                  <div>
+                    October 4, 11, 25
+                  </div>
+                  <div>
+                    November 8, 15, 22
+                  </div>  
+                </div>
+              </div>
+
+              <div class="q-mt-md">
+                <a style="text-decoration: none; color: #f0f9ff;" target="_blank" href="https://zoom.us/j/5255288358?pwd=T2QyNGgyRU9CcDBlK0REdHEvN0lyQT09">
+                  <q-btn outline label="Zoom App" class="full-width" />
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+       <div class="col-md-4 col-sm-6 col-12">
+        <div style="margin: 10px">
+            <div class="ZoomBoxes">
+              <div class="text-bold" style="font-size: 20px">
+                HPERDEV11
+                </div>
+                <div style="font-size: 10px">
+                  8:05 AM - 9:10 AM
+                </div>
+                <div style="font-size: 10px">
+                  Mondays
+                </div>
+
+              <div style="font-size: 13px">
+                <div class="q-mt-md">
+                  Meeting ID: 7752296634 <i class="far fa-copy" />
+                </div>
+                <div>
+                  Passcode: HPERDEV11 <i class="far fa-copy" />
+                </div>
+              </div>
+
+              <div class="text-center">
+                <div class="q-mt-md text-bold" style="font-size: 13px">
+                  DATES
+                </div>
+                <div style="font-size: 12px">
+                  <div> 
+                    September 13, 20, 27
+                  </div>
+                  <div>
+                    October 4, 11, 25
+                  </div>
+                  <div>
+                    November 8, 15, 22
+                  </div>  
+                </div>
+              </div>
+
+              <div class="q-mt-md">
+                <a style="text-decoration: none; color: #f0f9ff;" target="_blank" href="https://zoom.us/j/7752296634?pwd=WUdRYTZ1ZjRYcHE5OUZHN1dQU2Zvdz09">
+                  <q-btn outline label="Zoom App" class="full-width" />
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+         <div class="col-md-4 col-sm-6 col-12">
+        <div style="margin: 10px">
+            <div class="ZoomBoxes">
+              <div class="text-bold" style="font-size: 20px">
+                OLHMATH11
+                </div>
+                <div style="font-size: 10px">
+                  8:00 AM - 9:00 AM
+                </div>
+                <div style="font-size: 10px">
+                  Tuesdays
+                </div>
+
+              <div style="font-size: 13px">
+                <div class="q-mt-md">
+                  Meeting ID: 6590934495 <i class="far fa-copy" />
+                </div>
+                <div>
+                  Passcode: SHSicct <i class="far fa-copy" />
+                </div>
+              </div>
+
+              <div class="text-center">
+                <div class="q-mt-md text-bold" style="font-size: 13px">
+                  DATES
+                </div>
+                <div style="font-size: 12px">
+                  <div> 
+                    September 14, 21, 28
+                  </div>
+                  <div>
+                    October 5, 12, 26
+                  </div>
+                  <div>
+                    November 9, 16, 23
+                  </div>  
+                </div>
+              </div>
+
+              <div class="q-mt-md">
+                <a style="text-decoration: none; color: #f0f9ff;" target="_blank" href="https://zoom.us/j/6590934495?pwd=WUdRYTZ1ZjRYcHE5OUZHN1dQU2Zvdz09">
+                  <q-btn outline label="Zoom App" class="full-width" />
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+  <div class="col-md-4 col-sm-6 col-12">
+              <div style="margin: 10px">
+                  <div class="ZoomBoxes">
+                    <div class="text-bold" style="font-size: 20px">
+                      HFIL11
+                      </div>
+                      <div style="font-size: 10px">
+                        7:00 AM - 8:00 AM
+                      </div>
+                      <div style="font-size: 10px">
+                        Fridays
+                      </div>
+
+                    <div style="font-size: 13px">
+                      <div class="q-mt-md">
+                        Meeting ID: 95266896401 <i class="far fa-copy" />
+                      </div>
+                      <div>
+                        Passcode: SHS1234 <i class="far fa-copy" />
+                      </div>
+                    </div>
+
+                    <div class="text-center">
+                      <div class="q-mt-md text-bold" style="font-size: 13px">
+                        DATES
+                      </div>
+                      <div style="font-size: 12px">
+                        <div> 
+                          September 10, 17, 24
+                        </div>
+                        <div>
+                          October 1, 8, 15, 29
+                        </div>
+                        <div>
+                          November 12, 26
+                        </div>  
+                      </div>
+                    </div>
+
+                    <div class="q-mt-md">
+                      <a style="text-decoration: none; color: #f0f9ff;" target="_blank" href="https://zoom.us/j/95266896401?pwd=WUdRYTZ1ZjRYcHE5OUZHN1dQU2Zvdz09">
+                        <q-btn outline label="Zoom App" class="full-width" />
+                      </a>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+         <div class="col-md-4 col-sm-6 col-12">
+        <div style="margin: 10px">
+            <div class="ZoomBoxes">
+              <div class="text-bold" style="font-size: 20px">
+                OLHENG11
+                </div>
+                <div style="font-size: 10px">
+                  9:00 AM - 10:00 AM
                 </div>
                 <div style="font-size: 10px">
                   Fridays
@@ -17,10 +241,10 @@
 
               <div style="font-size: 13px">
                 <div class="q-mt-md">
-                  Meeting ID: 95266896401 <i class="far fa-copy" />
+                  Meeting ID: 5128036461 <i class="far fa-copy" />
                 </div>
                 <div>
-                  Passcode: SHS1234 <i class="far fa-copy" />
+                  Passcode: SHS4228 <i class="far fa-copy" />
                 </div>
               </div>
 
@@ -42,24 +266,40 @@
               </div>
 
               <div class="q-mt-md">
-                <a style="text-decoration: none; color: #f0f9ff;" target="_blank" href="https://zoom.us/j/95266896401?pwd=WUdRYTZ1ZjRYcHE5OUZHN1dQU2Zvdz09">
+                <a style="text-decoration: none; color: #f0f9ff;" target="_blank" href="https://zoom.us/j/5128036461?pwd=Wk9xRlN5SUF2UlVwVTd5YXBwa2pzdz09">
                   <q-btn outline label="Zoom App" class="full-width" />
                 </a>
               </div>
           </div>
         </div>
       </div>
-      
+
+
+
+
+
+
+
     </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import moment from 'moment'
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+    export default defineComponent({
+      name: 'PageIndex',
+
+      methods: {
+      currentDateTime() {
+        return moment().format('MMMM DD YYYY')
+      }
+    }
+    });
+
+
+    
 </script>
 
 
@@ -84,5 +324,13 @@ export default defineComponent({
   margin: 0 auto !important; 
 	float: none !important; 
   padding: 30px 30px 0px 30px
+}
+.dateToday {
+  border: 1px solid #a1a1a1;
+  padding: 15px 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  background-color: #002c4d;
+  border-radius: 4px;
 }
 </style>
